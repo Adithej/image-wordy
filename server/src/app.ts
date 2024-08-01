4d4969d235bc738b7f21b029882eb953c5a82c02
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-01 15:40:03 +0530

    domain change

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ config()
 const app = express()
 
 // app.use(cors({origin:"http://localhost:5173", credentials:true}))
-app.use(cors({origin:"https://frontend-wordy.netlify.app", credentials:true}))
+app.use(cors({origin:"https://imagewordy.netlify.app", credentials:true}))
 app.use(express.json({limit:"10mb"}))
 app.use(express.urlencoded({limit:"10mb", extended: true}))
 app.use(cookieParser(process.env.COOKIE_SECRET))
