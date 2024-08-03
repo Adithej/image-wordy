a0b11846bb0f9ecb07f7d03e71a2525f15d5db1b
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-03 22:06:47 +0530

    cookie set

--- server/src/controllers/userContollers.ts
+++ server/src/controllers/userContollers.ts
@@ -25,7 +25,7 @@ const cookieOptions = {
     signed: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     secure: true,
-    domain: "image-wordy-1.onrender.com",
+    domain: null,
 };
 
 export const userSignup = async (
