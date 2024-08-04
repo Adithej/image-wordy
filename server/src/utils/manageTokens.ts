24b2444b6b82689adfb24f169a853a83a432944c
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-04 11:50:47 +0530

    fix: verifyToken

--- server/src/utils/manageTokens.ts
+++ server/src/utils/manageTokens.ts
@@ -10,25 +10,18 @@ export const createToken = (id: string, email: string, expiresIn: string) => {
     return token;
 };
 
-export const verifyToken = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-    ) => {
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.signedCookies[`${COOKIE_NAME}`];
+
     if (!token || token.trim() === "") {
         return res.status(401).json({ message: "Token Not Received" });
     }
-    return new Promise<void>((resolve, reject) => {
-        return jwt.verify(token, process.env.JWT_SECRET, (error, success) => {
-        if (error) {
-            reject(error.message);
-            return res.status(401).json({ message: "Token Expired" });
-        } else {
-            resolve();
-            res.locals.jwtData = success;
-            return next();
-        }
-        });
-    });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        res.locals.jwtData = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Token Invalid" });
+    }
 };
